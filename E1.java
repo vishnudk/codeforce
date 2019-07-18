6f5f4b16ec92622ad2cb0723b329484f293d3b02
Vishnu <44140759+vishnudk@users.noreply.github.com>
2019-07-18 11:28:47 +0530

    Add files via upload

---
+++ E1.java
@@ -0,0 +1,9 @@
+public class E1
+{
+ public static void main(String arg[])
+ {
+   System.out.println("66 33 42 77 76 75 99 78");
+   System.out.println("33 42 66 75 76 77 78 99");
+   System.out.println("42 33 75 76 78 99 77 66");
+ }
+}
